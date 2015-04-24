d34577c092516dc44b42d10379903d8cde7c6452
Tomasz Trębski <kornicameister@gmail.com>
2015-04-25 01:23:31 +0200

    - implemented CRUD - implemented service

--- app/src/common/data/dataHelper.service.js
+++ app/src/common/data/dataHelper.service.js
@@ -1,15 +1,23 @@
 angular.module('todo.common.data')
-    .service('dataHelper', function () {
+    .service('dataHelper', function ($q, dataCrud) {
         var service = this;
 
         service.listCompleted = listCompleted.bind(service);
         service.deleteCompleted = deleteCompleted.bind(service);
 
         function listCompleted() {
-
+            return dataCrud.read({
+                completed: true
+            })
         }
 
         function deleteCompleted() {
+            return listCompleted().then(function (todos) {
+                var promises = _.transform(todos, function (accumulator, todo) {
+                    accumulator.push(dataCrud.remove(todo));
+                });
 
+                return $q.all(promises);
+            });
         }
     });
